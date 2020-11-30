49a64215522f7f8c535311003dc42e7766951ae4
Angelis <48072083+Angelion879@users.noreply.github.com>
2020-11-30 20:03:07 -0300

    Update Caesar.py

--- Caesar.py
+++ Caesar.py
@@ -1,5 +1,6 @@
 import math
 import string
+import curses.ascii
 import sys
 import os
 
@@ -9,24 +10,37 @@ def opReader ():
 	if ((op != '1') and (op != '2')):
 		sys.stdout.write('Please insert an valid option\n')
 		opReader()
-	keyReader()
+	return op
 
 def keyReader ():
 	#this reads the key to encode or decode
 	key = input('Please insert your numeric secret key: ')
 	for n in range(0, len(list(key)), 1):
-		if((key[n] < '0') or (key[n] > '9')):
+		if (curses.ascii.isalpha(key[n]) == 1):
 			sys.stdout.write('Please insert a valid Key\n')
 			keyReader()
-	print(int(key))
+	return int(key)
 	
 
-#def encrypt (K):
-	#this code encripts a message
+def encrypt (K):
+	#this code encrypts a message
+	message = input('What do you want to encrypt?\n')
+	for n in range(0, len(list(message))):
+		#this guarantee only letters will change
+		if (('Z' >= message[n] >= 'A') or ('z' >= message[n] >= 'a')):
+			#keep the loop with only alphabetic characters
+			while (K > 26):
+				K = abs(K - 26)
+			cipher = str(int(message[n]) + K)
+			sys.stdout.write(cipher)
+		else:
+			sys.stdout.write(message[n])
 
 #def decrypt (k):
 	#this code decrypts a message
 
 if __name__ == '__main__':
-	#This is the main code c: 
-	opReader()
+	#This is the main class c: 
+	option = opReader()
+	if option == '1':
+		encrypt(keyReader())
