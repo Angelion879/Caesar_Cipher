511f5cf0dd0fabc47c615a1ec1379117acd49824
Angelis <48072083+Angelion879@users.noreply.github.com>
2020-12-02 23:59:52 -0300

    Little changes

--- Caesar.py
+++ Caesar.py
@@ -3,21 +3,21 @@ import string
 import sys
 import os
 
-def opReader ():
+def op_Reader ():
 	#This read what the user wants 2 do
 	op = input('What do you want to do today?\nEncipher = 1\nDecipher = 2\n')
 	if (op in string.ascii_letters or (1 > int(op) or int(op) > 2)):
 		sys.stdout.write('Please insert an valid option\n')
-		opReader()
+		op_Reader()
 	return op
 
-def keyReader ():
+def key_Reader ():
 	#this reads the key to encode or decode
 	key = input('Please insert your numeric secret key: ')
 	for n in range(0, len(list(key)), 1):
 		if (key[n] in string.ascii_letters):
 			sys.stdout.write('Please insert a valid Key\n')
-			keyReader()
+			key_Reader()
 	#keep the loop with only alphabetic characters
 	if(int(key) > 26):
 		while (int(key) > 26):
@@ -25,7 +25,7 @@ def keyReader ():
 	return int(key)
 	
 
-def encrypt (K):
+def encrypt (k):
 	#this code encrypts a message
 	message = input('What do you want to encrypt?\n')
 	for n in range(0, len(list(message))):
@@ -33,11 +33,11 @@ def encrypt (K):
 		if (message[n] in string.ascii_letters):
 			if (message[n] in string.ascii_lowercase):
 				ALPHA = list(string.ascii_lowercase)
-				cipher = ALPHA.index(message[n]) + K
+				cipher = ALPHA.index(message[n]) + k
 				sys.stdout.write(string.ascii_lowercase[cipher])
 			elif (message[n] in string.ascii_uppercase):
 				ALPHA = list(string.ascii_uppercase)
-				cipher = ALPHA.index(message[n]) + K
+				cipher = ALPHA.index(message[n]) + k
 				sys.stdout.write(string.ascii_uppercase[cipher])
 		else:
 			sys.stdout.write(message[n])
@@ -47,6 +47,6 @@ def encrypt (K):
 
 if __name__ == '__main__':
 	#This is the main class c: 
-	option = opReader()
+	option = op_Reader()
 	if option == '1':
-		encrypt(keyReader())
+		encrypt(key_Reader())
